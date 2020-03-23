b650a20ed50e16dd48032027e62ee3b700f35f72
Daniel Binoy <dbinoy15@gmail.com>
2020-03-23 15:44:07 -0700

    its alive!

--- assets/js/todo-app.js
+++ assets/js/todo-app.js
@@ -1,7 +1,45 @@
+$(".delete-button").text("");
+$(".delete-button").css("width","0px");
 
-$( ".to-do-item" ).on( "click", function(){
+var last_item;
+
+$( ".to-do-list" ).on( "mouseover",".to-do-item", function(){
     console.log("mouse entered")
-    $(this).children(".delete-button").animate({
-        width:'toggle'
-    },350);
+    $(this).children(".delete-button").css("width","40px");
+    $(this).children(".delete-button").text("🗑");
+});
+
+$( ".to-do-list" ).on( "mouseout",".to-do-item", function(){
+    $(this).children(".delete-button").css("width","0px");
+    $(this).children(".delete-button").text("");
+});
+
+$( ".to-do-list" ).on( "click",".to-do-item", function(){
+    console.log("marked as completed");
+    $(this).children(".to-do-text").toggleClass("completed");
+});
+$( ".to-do-list" ).on( "click",".to-do-item .delete-button", function(){
+    console.log("deleted");
+    last_item = $(this).closest(".to-do-item").detach();
+});
+
+$("document").on("keydown",function(e){
+    if( e.which === 90 && e.ctrlKey && e.shiftKey ){
+        console.log('control + shift + z'); 
+    }
+    else if( e.which === 90 && e.ctrlKey ){
+        console.log('control + z'); 
+    } 
+});
+
+$('input[type="text"]').on("keypress",function(event){
+    if(event.which===13){
+       
+        var txt = '<li class = "to-do-item"><span class="delete-button">X</span><span class="to-do-text">'+$(this).val()+'</span></li>'
+        $(".to-do-list").append(txt)
+        
+        $(".delete-button").text("");
+        $(".delete-button").css("width","0px");
+        $(this).val("");
+    }
 });
